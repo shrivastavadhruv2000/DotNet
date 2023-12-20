b3d27288a9be8020faa339f1fd45d4436fe2d701
Dhruv Shrivastava <98174871+shrivastavadhruv2000@users.noreply.github.com>
2023-12-20 16:50:22 +0530

    Add files via upload

---
+++ DotNet/net/TestApp/Employee.cs
@@ -0,0 +1,31 @@
+namespace HR;
+public class Employee{
+    private int id;
+    private string firstName;
+
+    public int Id{
+        get{return this.id;}
+        set{this.id=value;}
+    }
+
+    public string FullName{
+        get;set;
+    }
+
+    public Employee():this(11,"Ashish Sharma"){
+        //this.id=11;
+        //this.fullNaame="Ashish Sharma
+    }
+
+    public Employee(int id,string fullName){
+        this.id=id;
+        this.FullName=fullName;
+    }
+
+    public override string ToString(){
+        return id+ " " +FullName;
+    }
+
+
+    
+}
---
+++ DotNet/net/TestApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using HR;
+Employee e1=new Employee();
+
+List<Employee> employees=new List<Employee>();
+employees.Add(e1);
+
+
+foreach(Employee emp in employees){
+    Console.WriteLine(emp);
+    //console.WriteLine(emp.)
+}
